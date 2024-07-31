206e12aa7ca64aded83f6b255ed4816bd4f3caa7
Shreya-454 <bshreya932@GMAIL.COM>
2024-07-31 10:10:39 +0530

    adding custom line-height

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,24 +5,27 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
   fontFamily:{
-    'poppins':["'Poppins',sans-serif"]
+    'poppins':"'Poppins',sans-serif"
+  },
+  fontSize: {
+    "custom-9xl":"80px"
   },
   colors:{
-    'dark-black':"#084CAF",
-    "dark-grey":"#4E4E4E",
-    "light-blue":"0A02EC14"
+    "dark-blue": "#084CAF",
+    "dark-grey": "#4E4E4E",
+    "light-blue": "0A02EC14",
+    "sky-blue": "#EBEBFD",
       },
       lineHeight:{
-        140:"140%",
-              },
-      container: {
+140:"140%",
+     "120":"120",
+        "150":"150%",
+      },
+    container: {
         center: true,
         padding: {
           DEFAULT: "20px",
